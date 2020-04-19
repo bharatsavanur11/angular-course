29f3f3d7f809ec6025cef176f51ead05f3c7895f
unknown <bharat.savanur@gmail.com>
2020-04-19 15:59:55 +0530

    Observables Part 1

--- week-3/src/app/services/dish.service.ts
+++ week-3/src/app/services/dish.service.ts
@@ -3,6 +3,8 @@ import {Dish} from '../shared/dish';
 import {DISHES} from '../shared/dishes';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
+import {delay} from 'rxjs/operators';
+import {of} from'rxjs';
 
 
 @Injectable({
@@ -13,26 +15,15 @@ export class DishService {
   constructor() { }
 
    getDishes(): Promise<Dish[]> {
-       return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES), 2000);
-    });
-
-      ;
+      return of(DISHES).pipe(delay(2000)).toPromise();
    }
 
    getDish(id: String) : Promise<Dish> {
-
-     return new Promise(resolve=>{
-        setTimeout(()=>resolve(DISHES.filter((dish)=> (dish.id === id))[0]),2000);
-     });
+     return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000)).toPromise();
    
    }
 
    getFeaturedDish(): Promise<Dish> {
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
-    });
+    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000)).toPromise();
   }
 }
